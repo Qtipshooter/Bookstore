80f672c11672847f459525afb25ac6383bb5ae3a
Quinton Graham <quintongraham1@gmail.com>
2025-08-15 12:30:54 -0700

    Adjusted gradient

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default async function Header(){
   return(
     <div className="flex justify-between sticky top-0 h-[10vh] w-full border-b-3 bg-secondary-bg">
-      <Link href="/" className="flex block h-full from-primary-bg bg-gradient-to-r pl-3">
-        <div className="m-auto text-6xl">Bookstore</div>
+      <Link href="/" className="flex block h-full from-primary-bg from-80% bg-gradient-to-r pl-4">
+        <div className="m-auto text-6xl pr-12">Bookstore</div>
       </Link>
       <Link href="/login" className="flex block mr-16">
         <div className="m-auto text-2xl p-2 border-2 rounded-lg underline">Signup/Sign In</div>
