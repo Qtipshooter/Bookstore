3c03571c6e2c784b19daeb8ad53cc28048dc30d1
Quinton Graham <quintongraham1@gmail.com>
2025-08-15 10:03:36 -0700

    Updated Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default async function Header(){
   return(
-    <div className="sticky top-0 h-[10vh] w-full border-b-3 text-center bg-secondary-bg text-secondary-bg-text">
-      Headers
+    <div className="flex justify-between sticky top-0 h-[10vh] w-full border-b-3 bg-secondary-bg">
+      <Link href="/" className="flex block h-full from-primary-bg bg-gradient-to-r pl-3">
+        <div className="m-auto text-6xl">Bookstore</div>
+      </Link>
+      <Link href="/login" className="flex block mr-16">
+        <div className="m-auto text-2xl p-2 border-2 rounded-lg underline">Signup/Sign In</div>
+      </Link>
+      
     </div>
   );
 }
