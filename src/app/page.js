c1812438a67ece25ad7da6b4077f411afd9567ba
Quinton Graham <quintongraham1@gmail.com>
2025-09-03 11:25:44 -0700

    Design fleshing and link updates

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,14 @@ import Image from "next/image";
 export default async function Home() {
   return (
     <div>
-      <div className="h-[80vh] bg-cover bg-center bg-white/60 bg-[url(/images/books.jpeg)] bg-blend-overlay bg-opacity-5 text-primary-bg-text text-center">
-        Body
+      <div className="min-h-[80vh] px-[10vw] bg-cover bg-center bg-white/60 bg-[url(/images/books.jpeg)] bg-blend-overlay bg-opacity-5 flex justify-evenly gap-6 flex-wrap">
+        <div className="m-auto text-8xl">
+          <div className="mt-[5vh]">Bookstore</div>
+        </div>
+        <div className="m-auto max-w-[40vw] flex flex-col gap-6 bg-linear-to-r to-secondary-bg/80 from-primary-bg/80 p-6 border-transparent rounded-4xl">
+          <div className="indent-8 text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed felis diam, posuere at fringilla nec, varius eu sapien. Phasellus elementum augue ut lectus rhoncus, aliquet volutpat enim consequat. Nulla quis feugiat tortor, sed viverra mi. Aliquam porta, elit sit amet congue vestibulum, tellus magna condimentum nulla, et egestas augue nisl mattis massa. Vivamus rhoncus, tortor eget aliquet vestibulum, quam est posuere nunc, sit amet euismod orci lorem ac augue. Suspendisse potenti. Vivamus in augue lectus. Nam placerat lacus tortor, sit amet scelerisque augue fringilla a. Pellentesque vitae sodales massa. Aliquam erat volutpat. Maecenas et pulvinar purus. Aenean eget hendrerit neque.</div>
+          <div className="indent-8 text-justify">Aenean non posuere arcu. Ut hendrerit ligula quis justo luctus, sed ullamcorper ante euismod. Nam sed tellus nec neque pretium rutrum. Donec at ante elit. Pellentesque a augue leo. Pellentesque magna elit, vehicula et viverra vel, pretium dapibus felis. Morbi ac erat auctor, pharetra magna vitae, lacinia risus. Pellentesque et magna risus. Morbi id luctus leo, ut consectetur ante. Duis finibus tempus orci, a malesuada dui facilisis id. Nunc eu mi augue.</div>
+        </div>
       </div>
       <div className="flex flex-col p-4">
         <div className="text-center h-16 text-3xl font-bold">Popular Books on Sale</div>
--- src/components/footer.js
+++ src/components/footer.js
@@ -4,23 +4,23 @@ export default async function Footer() {
   return (
     <div className="flex justify-between px-[10vw] py-4 bg-dark-bg text-dark-bg-text w-full border-t-3 border-blue-300 min-h-[10vh] text-center">
       <div className="h-auto m-auto flex flex-col">
-        <div className="text-4xl underline">Bookstore</div>
-        <Link className="hover:underline" href="/">About Us</Link>
+        <Link className="text-4xl underline" href="/">Bookstore</Link>
+        <Link className="hover:underline" href="/about">About Us</Link>
       </div>
       <div className="h-auto mx-auto flex flex-col">
-        <div className="text-xl border-b-1 mb-2">Quick Searches</div>
-        <Link className="hover:underline" href="/">Non-Fiction</Link>
-        <Link className="hover:underline" href="/">Adventure</Link>
-        <Link className="hover:underline" href="/">Children's</Link>
-        <Link className="hover:underline" href="/">Romance</Link>
-        <Link className="hover:underline" href="/">Sci-Fi</Link>
+        <div className="text-xl border-b-1 mb-2 px-2">Quick Searches</div>
+        <Link className="hover:underline" href="/browse">Non-Fiction</Link>
+        <Link className="hover:underline" href="/browse">Adventure</Link>
+        <Link className="hover:underline" href="/browse">Children's</Link>
+        <Link className="hover:underline" href="/browse">Romance</Link>
+        <Link className="hover:underline" href="/browse">Sci-Fi</Link>
       </div>
       <div className="h-auto mx-auto flex flex-col">
-        <div className="text-xl border-b-1 mb-2">Nav Links</div>
-        <Link className="hover:underline" href="/">Account</Link>
-        <Link className="hover:underline" href="/">Browse</Link>
-        <Link className="hover:underline" href="/">Cart</Link>
-        <Link className="hover:underline" href="/">My Orders</Link>
+        <div className="text-xl border-b-1 mb-2 px-2">Nav Links</div>
+        <Link className="hover:underline" href="/account">Account</Link>
+        <Link className="hover:underline" href="/browse">Browse</Link>
+        <Link className="hover:underline" href="/cart">Cart</Link>
+        <Link className="hover:underline" href="/orders">My Orders</Link>
       </div>
     </div>
   );
